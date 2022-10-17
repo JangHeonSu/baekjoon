91604fc6cdec65549a605658fb02d473d554cd54
J-malone <102356330+jheonsu@users.noreply.github.com>
2022-10-17 20:45:01 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main() {
+
+    int n,k=0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        k += i;
+    }
+
+    printf("%d", k);
+
+    return 0;
+}
