a7ae0694d7f24f8650ec6d262f11a6f220b780be
J-malone <102356330+jheonsu@users.noreply.github.com>
2022-10-23 01:28:13 +0900

    Update 부녀회장이 될테야.cc

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,34 +1,61 @@
 #include <cstdio>
-void apart(int k, int n,int arr[]) {
 
-	for (int i = 0; i < k; i++) {
-		int temp = 0;
-		int j = 0;
-		while (j<n) {
-			temp += arr[j];
-			arr[j] = temp;
-			j++;
+int main()
+{
+	int T, k, n;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d", &k);
+		scanf("%d", &n);
+		int num1[16] = { 0, };
+		int num2[16] = { 0, };
+		int count = 1;
+		for (int j = 0; j <= n; j++)
+		{
+			for (int t = 0; t <= j; t++)
+			{
+				num1[j] += t;
+			}
 		}
-	}
-}
+		while (1)
+		{
+			if (k == count)
+				break;
+			for (int j = 0; j <= n; j++)
+			{
+				num2[j] = 0;
+			}
+			for (int j = 0; j <= n; j++)
+			{
+				for (int t = 0; t <= j; t++)
+				{
+					num2[j] += num1[t];
+				}
+			}
+			count++;
+			if (k == count)
+				break;
 
-int main(void) {
+			for (int j = 0; j <= n; j++)
+			{
+				num1[j] = 0;
+			}
 
-	int t;
-	int arr[14]={0,};
-
-	scanf("%d", &t);
-
-	for (int i = 0; i < t; i++) {
-
-		int k, n;
-		scanf("%d %d", &k, &n);
-
-		for (int j = 0; j < n; j++) 
-			arr[j] = j+1;
-
-			apart(k, n, arr);
-			printf("%d\n", arr[n - 1]);
+			for (int j = 0; j <= n; j++)
+			{
+				for (int t = 0; t <= j; t++)
+				{
+					num1[j] += num2[t];
+				}
+			}
+			count++;
+		}
+		if (k % 2 == 0)
+			printf("%d\n", num2[n]);
+		else
+			printf("%d\n", num1[n]);
 	}
 
 	return 0;
