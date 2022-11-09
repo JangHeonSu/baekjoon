ebc0dddc32df7e930eb27d059f215a16b4b407a9
J-malone <102356330+jheonsu@users.noreply.github.com>
2022-11-09 15:43:49 +0900

    [Silver V] Title: 나이순 정렬, Time: 108 ms, Memory: 9732 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
-bool compare(pair<int,string> a, pair<int,string> b)
+
+//struct hs {
+//	int age=0;
+//	string name="\n";
+//}x;
+
+bool comp(pair<int, string> i, pair<int, string> j)
 {
-    return a.first < b.first;
+	return i.first < j.first;
 }
+
 int main() {
-    int num;
-    cin >> num;
-    pair<int,string> tmp;
-    vector<pair<int,string>> arr;
-    for(int i = 0; i < num; i++)
-    {
-        cin >> tmp.first >> tmp.second;
-        arr.push_back(tmp);
-    }
-    stable_sort(arr.begin(),arr.end(),compare);
-    for(int i = 0; i < num; i++)
-        cout << arr[i].first << ' ' << arr[i].second << '\n';
+
+	int N;
+	cin >> N;
+	pair<int, string> tmp;
+    vector<pair<int,string>> v;
+	for (int i=0;i<N;i++)
+	{
+		cin >> tmp.first >> tmp.second;
+		v.push_back(tmp);
+	}
+
+	stable_sort(v.begin(), v.end(),comp);
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+
+	return 0;
 }
