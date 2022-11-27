089f4059e3670b4f5055d4aeece4df2998c325c6
J-malone <102356330+jheonsu@users.noreply.github.com>
2022-11-28 04:59:56 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14159265359
+
+int main() {
+
+	double R;
+	cin >> R;
+	cout << fixed;
+	cout << R * R * PI<<'\n';
+	cout << R * R * 2 << '\n';
+
+	
+	return 0;
+}
